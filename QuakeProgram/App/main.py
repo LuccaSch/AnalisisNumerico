8382b1d5c6fc27952dcdad4db4d393adbe97413f
Lucca <lucca.schiaffino@gmail.com>
2024-04-23 14:32:59 -0300

    1.0.3

--- QuakeProgram/App/main.py
+++ QuakeProgram/App/main.py
@@ -1,5 +1,6 @@
 import pathlib
 import numpy as np
+import os
 
 from numpy import array
 from matplotlib import pyplot as plt
@@ -20,6 +21,32 @@ def mostrarGrafica(t,ft,nombre,trafo_fourier,frecuencia):
  plt.show()
  return
 
+def calcular_distancia_fourier(terremoto1, terremoto2):
+    # Calcula la diferencia entre los espectros de frecuencia
+    diferencia_espectros = np.abs(terremoto1 - terremoto2)
+    # Calcula la distancia euclidiana entre los espectros de frecuencia
+    distancia = np.linalg.norm(diferencia_espectros)
+    return distancia
+
+def directorio_vacio(ruta):
+    # Obtener la lista de archivos y directorios en la ruta especificada
+    contenido = os.listdir(ruta)
+    
+    # Verificar si la lista está vacía
+    if len(contenido) == 0:
+        return True
+    else:
+        return False
+
+def obtener_archivos_en_directorio(ruta):
+    # Obtener la lista de archivos y directorios en la ruta especificada
+    contenido = os.listdir(ruta)
+    
+    # Retornar solo los archivos (excluir directorios)
+    archivos = [archivo for archivo in contenido if os.path.isfile(os.path.join(ruta, archivo))]
+    
+    return archivos
+
 def guardar_arreglo_en_txt(arreglo, nombre_archivo):
     with open(nombre_archivo, 'w') as archivo:
         for elemento in arreglo:
@@ -29,38 +56,30 @@ ruta = pathlib.Path('.')
 
 ruta = ruta / "QuakeProgram"
 
-print('Buscar el archivo que desea leer')
+imp= ruta / "Imputs"
 
-respuesta = input('Ingrese el nombre del archivo [exit para salir]: ')
-nombre = respuesta
-
-archivo = ruta / "Imputs" / respuesta
-
-while respuesta!="exit":
-    if archivo.exists():
-        #Ejer 1
-        datos = np.loadtxt(archivo) #Descomprimimos el txt en una lista donde el primer elemento es el t y el segundo elemento el Ft, guarados en arreglos .np
-        t= datos[:, 0]
-        ft=datos[:, 1]
+if(directorio_vacio(imp)):
+    print("Debe cargar Imputs para analisar")
+else: 
+    archivo=obtener_archivos_en_directorio(imp)
+    funciones_de_t= []
+    frecuencias=[]
+    coeficientes_de_fourier=[]
+    transformadas_de_fouerier=[]
+    
+    for indice in archivo:
+        print(indice)
+        direc = ruta / "Imputs" / indice
+        archivo_output=ruta / "Outputs" / indice
+        
+        #1
+        funcion_t= np.loadtxt(direc)
+        t= funcion_t[:, 0]
+        ft=funcion_t[:, 1]
         coeficiente_fourier = np.fft.fft(ft)/len(t) #Sacamos los coeficientes de la serie de fourier para cada pto, lo dividimos por el ancho de la entrada que seria el 1/N que esplicaron en clase
-        trafo_fourier= abs(coeficiente_fourier) 
-        
+        trafo_fourier= abs(coeficiente_fourier)
         frecuencia = np.fft.fftfreq(len(t),t[0]-t[1])
+    
+        guardar_arreglo_en_txt(coeficiente_fourier,archivo_output)
         
-        archivo_output= ruta / "Outputs" / "coeficientes1.txt" #generamos una ruta a Outputs
-        guardar_arreglo_en_txt(coeficiente_fourier,archivo_output)#guardamos todos los coeficientes de fourier en outputs
-        
-        mostrarGrafica(t,ft,nombre,trafo_fourier,frecuencia) #mostramos la grafica de la trasformada
-        #Ejer 2
-        
-        
-        respuesta = "exit" #salimos del while
-    else:
-        #Caso de ingresar mal el archivo
-        print('No existe el archivo seleccionado')
-        
-        respuesta = input('Ingrese el nombre del archivo [exit para salir]: ')
-        
-        archivo = ruta / "Imputs" / respuesta
-        
-        nombre = respuesta
+        mostrarGrafica(t,ft,indice,trafo_fourier,frecuencia) #mostramos la grafica de la trasformada
